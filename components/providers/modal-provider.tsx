e259713ff628df7706574195e5f05a3d9f6bf612
Aditya <akaditya394@gmail.com>
2023-10-09 14:15:47 +0530

    update server functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import CreateServerModal from "../modals/create-server-modal";
 import InviteModal from "../modals/invite-modal";
+import EditServerModal from "../modals/edit-server-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +19,7 @@ const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 };
