79e29b97663d89c74c565a25548683f58ab877af
Aditya <akaditya394@gmail.com>
2023-10-09 22:14:01 +0530

    delete & leave server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,8 @@ import InviteModal from "../modals/invite-modal";
 import EditServerModal from "../modals/edit-server-modal";
 import MembersModal from "../modals/members-modal";
 import CreateChannel from "../modals/create-channel-modal";
+import LeaveServerModal from "../modals/leave-server-modal";
+import DeleteServerModal from "../modals/delete-server-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -24,6 +26,8 @@ const ModalProvider = () => {
       <EditServerModal />
       <MembersModal />
       <CreateChannel />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
