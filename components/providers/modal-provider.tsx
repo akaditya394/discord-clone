d7ba3f582aef0488eacb0c8a90ff1e7b26b7fe08
Aditya <akaditya394@gmail.com>
2023-10-12 17:00:37 +0530

    message attachment

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -9,6 +9,7 @@ import DeleteServerModal from "../modals/delete-server-modal";
 import { DeleteChannelModal } from "../modals/delete-channel-modal";
 import { EditChannelModal } from "../modals/edit-channel-modal";
 import { CreateServerModal } from "../modals/create-server-modal";
+import MessageFileModal from "../modals/message-file-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -32,6 +33,7 @@ const ModalProvider = () => {
       <DeleteServerModal />
       <DeleteChannelModal />
       <EditChannelModal />
+      <MessageFileModal />
     </>
   );
 };
